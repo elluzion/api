5c0b16122b539bc00dee8c0b3c7258f92bd721fb
Fynn <fynnruebenach@gmail.com>
2024-07-30 20:55:08 +0200

    🐛 Dont export API itself

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,26 @@ import { SoundcloudController } from './controllers/soundcloud-controller';
 
 const PORT = process.env.PORT || 3000;
 
-const api = new Elysia().use(SoundcloudController).use(
-  swagger({
-    version: '1.0.0',
-    path: '/docs',
-    documentation: {
-      info: {
-        title: 'Tools API',
-        version: '1.0.0',
+const api = new Elysia()
+  .use(SoundcloudController)
+  .use(
+    swagger({
+      version: '1.0.0',
+      path: '/docs',
+      documentation: {
+        info: {
+          title: 'Tools API',
+          version: '1.0.0',
+        },
       },
-    },
-    theme: 'dark',
-    scalarConfig: {
-      theme: 'purple',
-    },
-  }),
-);
-
-api.listen(PORT);
+      theme: 'dark',
+      scalarConfig: {
+        theme: 'purple',
+      },
+    }),
+  )
+  .listen(PORT);
 
 console.log(`Listening on port ${PORT}`);
 
 export type Api = typeof api;
-export default api;
